f71d48388aeb45efac1f725894b701fd4ee4176b
Ajiny <91933032+Ajiny@users.noreply.github.com>
2022-10-03 01:06:08 -0700

    direction added

---
+++ UnityBasicChan/Assets/Assets/Movement.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public KeyCode jumpKey;
+
+    public float speed = 10f;
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        // "w" can be replaced with any key
+        // this section moves the character up
+        if (Input.GetKey("w"))
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+
+        // "s" can be replaced with any key
+        // this section moves the character down
+        if (Input.GetKey("s"))
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+
+        // "d" can be replaced with any key
+        // this section moves the character right
+        if (Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+
+        // "a" can be replaced with any key
+        // this section moves the character left
+        if (Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+       
+        // added movement for diagonal direction
+        // "t" can be replaced with any key
+        // this section moves the character left
+        double m = 0.8;
+        float diag = (float) m;
+
+        if (Input.GetKey("t"))
+        {
+            pos.x -= diag * speed * Time.deltaTime;
+            pos.y += diag * speed * Time.deltaTime;
+        }
+
+        // added movement for diagonal direction
+        // "z" can be replaced with any key
+        // this section moves the character left
+        if (Input.GetKey("z"))
+        {
+            pos.x += diag * speed * Time.deltaTime;
+            pos.y -= diag * speed * Time.deltaTime;
+        }
+
+
+        transform.position = pos;
+    }
+}
